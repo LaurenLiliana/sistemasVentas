0a22881104aa7269007c6c1734cc0c2cd3c10910
elkins33 <gamesold33@gmail.com>
2024-08-11 19:33:31 -0600

    Trabajado hasta la implementacion del modulo de venta

--- SistemaVenta.IOC/Dependencia.cs
+++ SistemaVenta.IOC/Dependencia.cs
@@ -37,6 +37,9 @@ namespace SistemaVenta.IOC
             services.AddScoped<IUsuarioService, UsuarioService>();
             services.AddScoped<INegocioService, NegocioService>();
             services.AddScoped<ICategoriaService, CategoriaService>();
+            services.AddScoped<IProductoService, ProductoService>();
+            services.AddScoped<ITipoDocumentoVentaService, TipoDocumentoVentaService>();
+            services.AddScoped<IVentaService, VentaService>();
 
         } 
     }
